2fef552a6b64ae7a2b80d060c9d9ddda0f97333a
Jeffrey Patton <jeffrey@patton-tech.com>
2022-02-15 17:37:20 -0600

    Significant changes, overhauled the entire thing as a module

---
+++ GetTemplate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.IO;
+using System.Net.Http;
+
+
+namespace framework
+{
+    [Cmdlet(VerbsCommon.Get, "Template")]
+    public class GetTemplate : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string Path
+        {
+            get { return _Path; }
+            set { _Path = value; }
+        }
+        private string _Path;
+        protected override async void ProcessRecord()
+        {
+            UriBuilder uriBuilder = new UriBuilder(Path);
+            string result = "";
+
+            if (uriBuilder.Scheme == "file")
+            {
+                WriteVerbose("filePath: " + Path);
+
+                result = File.ReadAllText(Path);
+            }
+            else if (uriBuilder.Scheme == "http" || uriBuilder.Scheme == "https")
+            {
+                WriteVerbose("urlPath: " + uriBuilder.Uri.AbsoluteUri);
+                
+                using HttpClient httpClient = new HttpClient();
+                HttpResponseMessage response = await httpClient.GetAsync(uriBuilder.Uri.AbsoluteUri);
+                response.EnsureSuccessStatusCode();
+                result = await response.Content.ReadAsStringAsync();
+
+            }
+            WriteObject(result);
+            base.ProcessRecord();
+        }
+    }
+}
